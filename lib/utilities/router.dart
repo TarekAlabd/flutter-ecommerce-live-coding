f7603b890d0a259f1e1bbca8e8697e3c53d74068
Tarek Alabd <tarekalabd1@gmail.com>
2022-06-03 17:45:56 +0200

    [FEATURE] add the ui of the login and register pages

---
+++ lib/utilities/router.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_ecommerce/utilities/routes.dart';
+import 'package:flutter_ecommerce/views/pages/landing_page.dart';
+import 'package:flutter_ecommerce/views/pages/auth_page.dart';
+
+Route<dynamic> onGenerate(RouteSettings settings) {
+  switch (settings.name) {
+    case AppRoutes.loginPageRoute:
+      return CupertinoPageRoute(
+        builder: (_) => const AuthPage(),
+      );
+    case AppRoutes.landingPageRoute:
+    default:
+      return CupertinoPageRoute(
+        builder: (_) => const LandingPage(),
+      );
+  }
+}
---
+++ lib/views/pages/landing_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
