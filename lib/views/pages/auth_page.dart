81fe892a50ea0b604f2cc20598062055f9e72efb
Tarek Alabd <tarekalabd1@gmail.com>
2022-06-10 18:21:55 +0200

    [FEATURE] edit some changes in the auth pages and finished the home page ui

--- lib/views/pages/auth_page.dart
+++ lib/views/pages/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/utilities/enums.dart';
+import 'package:flutter_ecommerce/utilities/routes.dart';
 import 'package:flutter_ecommerce/views/widgets/main_button.dart';
 
 class AuthPage extends StatefulWidget {
@@ -13,11 +14,22 @@ class _AuthPageState extends State<AuthPage> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
   var _authType = AuthFormType.login;
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(
@@ -26,96 +38,116 @@ class _AuthPageState extends State<AuthPage> {
           ),
           child: Form(
             key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  _authType == AuthFormType.login ? 'Login' : 'Register',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                const SizedBox(height: 80.0),
-                TextFormField(
-                  controller: _emailController,
-                  validator: (val) =>
-                      val!.isEmpty ? 'Please enter your email!' : null,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email!',
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    _authType == AuthFormType.login ? 'Login' : 'Register',
+                    style: Theme.of(context).textTheme.headline4,
                   ),
-                ),
-                const SizedBox(height: 24.0),
-                TextFormField(
-                  controller: _passwordController,
-                  validator: (val) =>
-                      val!.isEmpty ? 'Please enter your password!' : null,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your pasword!',
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                if (_authType == AuthFormType.login)
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: InkWell(
-                      child: const Text('Forgot your password?'),
-                      onTap: () {},
+                  const SizedBox(height: 80.0),
+                  TextFormField(
+                    controller: _emailController,
+                    focusNode: _emailFocusNode,
+                    onEditingComplete: () =>
+                        FocusScope.of(context).requestFocus(_passwordFocusNode),
+                    textInputAction: TextInputAction.next,
+                    validator: (val) =>
+                        val!.isEmpty ? 'Please enter your email!' : null,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email!',
                     ),
                   ),
-                const SizedBox(height: 24.0),
-                MainButton(
-                  text: _authType == AuthFormType.login ? 'Login' : 'Register',
-                  onTap: () {},
-                ),
-                const SizedBox(height: 16.0),
-                Align(
-                  alignment: Alignment.center,
-                  child: InkWell(
-                    child: Text(_authType == AuthFormType.login ? 'Don\'t have an account? Register' : 'Have an account? Login',),
+                  const SizedBox(height: 24.0),
+                  TextFormField(
+                    controller: _passwordController,
+                    focusNode: _passwordFocusNode,
+                    validator: (val) =>
+                        val!.isEmpty ? 'Please enter your password!' : null,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your pasword!',
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  if (_authType == AuthFormType.login)
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: InkWell(
+                        child: const Text('Forgot your password?'),
+                        onTap: () {},
+                      ),
+                    ),
+                  const SizedBox(height: 24.0),
+                  MainButton(
+                    text:
+                        _authType == AuthFormType.login ? 'Login' : 'Register',
                     onTap: () {
-                      setState(() {
-                        if (_authType == AuthFormType.login) {
-                          _authType = AuthFormType.register;
-                        } else {
-                          _authType = AuthFormType.login;
-                        }
-                      });
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.of(context)
+                            .pushNamed(AppRoutes.bottomNavBarRoute);
+                      }
                     },
                   ),
-                ),
-                const Spacer(),
-                Align(
+                  const SizedBox(height: 16.0),
+                  Align(
                     alignment: Alignment.center,
-                    child: Text(
-                      _authType == AuthFormType.login ? 'Or Login with' : 'Or Register with',
-                      style: Theme.of(context).textTheme.subtitle1,
-                    )),
-                const SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 80,
-                      width: 80,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16.0),
-                        color: Colors.white,
+                    child: InkWell(
+                      child: Text(
+                        _authType == AuthFormType.login
+                            ? 'Don\'t have an account? Register'
+                            : 'Have an account? Login',
                       ),
-                      child: const Icon(Icons.add),
+                      onTap: () {
+                        _formKey.currentState!.reset();
+                        setState(() {
+                          if (_authType == AuthFormType.login) {
+                            _authType = AuthFormType.register;
+                          } else {
+                            _authType = AuthFormType.login;
+                          }
+                        });
+                      },
                     ),
-                    const SizedBox(width: 16.0),
-                    Container(
-                      height: 80,
-                      width: 80,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16.0),
-                        color: Colors.white,
+                  ),
+                  SizedBox(height: size.height * 0.09),
+                  Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        _authType == AuthFormType.login
+                            ? 'Or Login with'
+                            : 'Or Register with',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      )),
+                  const SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          color: Colors.white,
+                        ),
+                        child: const Icon(Icons.add),
                       ),
-                      child: const Icon(Icons.add),
-                    ),
-                  ],
-                ),
-              ],
+                      const SizedBox(width: 16.0),
+                      Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          color: Colors.white,
+                        ),
+                        child: const Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
