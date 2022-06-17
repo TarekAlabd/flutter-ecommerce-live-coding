f790065b96adbb532b7e248a8ce63ea03adbea40
Tarek Alabd <tarekalabd1@gmail.com>
2022-06-17 19:37:41 +0200

    Day3 17/6 auth firebase (#7)
    * [DEPENDECIES] integrating with firebase
    
    * [FEATURE] finalize the integration with firebase and the project
    
    * [ENHANCEMENT] add the landing page to save the state of the logged in user
    
    * [FIX] fix the issue with the api key and prevent the google services file from publishing to Github
    
    * Delete google-services.json
    
    * Delete GoogleService-Info.plist

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/services/auth.dart';
 import 'package:flutter_ecommerce/utilities/router.dart';
 import 'package:flutter_ecommerce/utilities/routes.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,47 +17,50 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ecommerce App',
-      theme: ThemeData(
-          scaffoldBackgroundColor: const Color(0xFFE5E5E5),
-          primaryColor: Colors.red,
-          inputDecorationTheme: InputDecorationTheme(
-            labelStyle: Theme.of(context).textTheme.subtitle1,
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(2.0),
-              borderSide: const BorderSide(
-                color: Colors.grey,
+    return Provider<AuthBase>(
+      create: (_) => Auth(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Ecommerce App',
+        theme: ThemeData(
+            scaffoldBackgroundColor: const Color(0xFFE5E5E5),
+            primaryColor: Colors.red,
+            inputDecorationTheme: InputDecorationTheme(
+              labelStyle: Theme.of(context).textTheme.subtitle1,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(2.0),
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                ),
               ),
-            ),
-            disabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(2.0),
-              borderSide: const BorderSide(
-                color: Colors.grey,
+              disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(2.0),
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                ),
               ),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(2.0),
-              borderSide: const BorderSide(
-                color: Colors.grey,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(2.0),
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                ),
               ),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(2.0),
-              borderSide: const BorderSide(
-                color: Colors.red,
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(2.0),
+                borderSide: const BorderSide(
+                  color: Colors.red,
+                ),
               ),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(2.0),
-              borderSide: const BorderSide(
-                color: Colors.red,
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(2.0),
+                borderSide: const BorderSide(
+                  color: Colors.red,
+                ),
               ),
-            ),
-          )),
-      onGenerateRoute: onGenerate,
-      initialRoute: AppRoutes.loginPageRoute,
+            )),
+        onGenerateRoute: onGenerate,
+        initialRoute: AppRoutes.landingPageRoute,
+      ),
     );
   }
 }
--- lib/views/pages/auth_page.dart
+++ lib/views/pages/auth_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/controllers/auth_controller.dart';
 import 'package:flutter_ecommerce/utilities/enums.dart';
 import 'package:flutter_ecommerce/utilities/routes.dart';
 import 'package:flutter_ecommerce/views/widgets/main_button.dart';
+import 'package:provider/provider.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -16,7 +18,6 @@ class _AuthPageState extends State<AuthPage> {
   final _passwordController = TextEditingController();
   final _emailFocusNode = FocusNode();
   final _passwordFocusNode = FocusNode();
-  var _authType = AuthFormType.login;
 
   @override
   void dispose() {
@@ -25,133 +26,166 @@ class _AuthPageState extends State<AuthPage> {
     super.dispose();
   }
 
+  Future<void> _submit(AuthController model) async {
+    try {
+      await model.submit();
+      if (!mounted) return;
+      Navigator.of(context).pushNamed(AppRoutes.bottomNavBarRoute);
+    } catch (e) {
+      // TODO: We will refactor this code into another widget in the next session
+      showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text(
+            'Error!',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          content: Text(
+            e.toString(),
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 60.0,
-            horizontal: 32.0,
-          ),
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _authType == AuthFormType.login ? 'Login' : 'Register',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  const SizedBox(height: 80.0),
-                  TextFormField(
-                    controller: _emailController,
-                    focusNode: _emailFocusNode,
-                    onEditingComplete: () =>
-                        FocusScope.of(context).requestFocus(_passwordFocusNode),
-                    textInputAction: TextInputAction.next,
-                    validator: (val) =>
-                        val!.isEmpty ? 'Please enter your email!' : null,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      hintText: 'Enter your email!',
-                    ),
-                  ),
-                  const SizedBox(height: 24.0),
-                  TextFormField(
-                    controller: _passwordController,
-                    focusNode: _passwordFocusNode,
-                    validator: (val) =>
-                        val!.isEmpty ? 'Please enter your password!' : null,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter your pasword!',
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  if (_authType == AuthFormType.login)
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: InkWell(
-                        child: const Text('Forgot your password?'),
-                        onTap: () {},
-                      ),
-                    ),
-                  const SizedBox(height: 24.0),
-                  MainButton(
-                    text:
-                        _authType == AuthFormType.login ? 'Login' : 'Register',
-                    onTap: () {
-                      if (_formKey.currentState!.validate()) {
-                        Navigator.of(context)
-                            .pushNamed(AppRoutes.bottomNavBarRoute);
-                      }
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  Align(
-                    alignment: Alignment.center,
-                    child: InkWell(
-                      child: Text(
-                        _authType == AuthFormType.login
-                            ? 'Don\'t have an account? Register'
-                            : 'Have an account? Login',
-                      ),
-                      onTap: () {
-                        _formKey.currentState!.reset();
-                        setState(() {
-                          if (_authType == AuthFormType.login) {
-                            _authType = AuthFormType.register;
-                          } else {
-                            _authType = AuthFormType.login;
-                          }
-                        });
-                      },
-                    ),
-                  ),
-                  SizedBox(height: size.height * 0.09),
-                  Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        _authType == AuthFormType.login
-                            ? 'Or Login with'
-                            : 'Or Register with',
-                        style: Theme.of(context).textTheme.subtitle1,
-                      )),
-                  const SizedBox(height: 16.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+
+    return Consumer<AuthController>(
+      builder: (_, model, __) {
+        return Scaffold(
+          resizeToAvoidBottomInset: true,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 60.0,
+                horizontal: 32.0,
+              ),
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        height: 80,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16.0),
-                          color: Colors.white,
-                        ),
-                        child: const Icon(Icons.add),
+                      Text(
+                        model.authFormType == AuthFormType.login
+                            ? 'Login'
+                            : 'Register',
+                        style: Theme.of(context).textTheme.headline4,
                       ),
-                      const SizedBox(width: 16.0),
-                      Container(
-                        height: 80,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16.0),
-                          color: Colors.white,
+                      const SizedBox(height: 80.0),
+                      TextFormField(
+                        controller: _emailController,
+                        focusNode: _emailFocusNode,
+                        onEditingComplete: () => FocusScope.of(context)
+                            .requestFocus(_passwordFocusNode),
+                        textInputAction: TextInputAction.next,
+                        onChanged: model.updateEmail,
+                        validator: (val) =>
+                            val!.isEmpty ? 'Please enter your email!' : null,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter your email!',
                         ),
-                        child: const Icon(Icons.add),
+                      ),
+                      const SizedBox(height: 24.0),
+                      TextFormField(
+                        controller: _passwordController,
+                        focusNode: _passwordFocusNode,
+                        validator: (val) =>
+                            val!.isEmpty ? 'Please enter your password!' : null,
+                        onChanged: model.updatePassword,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter your pasword!',
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      if (model.authFormType == AuthFormType.login)
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: InkWell(
+                            child: const Text('Forgot your password?'),
+                            onTap: () {},
+                          ),
+                        ),
+                      const SizedBox(height: 24.0),
+                      MainButton(
+                        text: model.authFormType == AuthFormType.login
+                            ? 'Login'
+                            : 'Register',
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            _submit(model);
+                          }
+                        },
+                      ),
+                      const SizedBox(height: 16.0),
+                      Align(
+                        alignment: Alignment.center,
+                        child: InkWell(
+                          child: Text(
+                            model.authFormType == AuthFormType.login
+                                ? 'Don\'t have an account? Register'
+                                : 'Have an account? Login',
+                          ),
+                          onTap: () {
+                            _formKey.currentState!.reset();
+                            model.toggleFormType();
+                          },
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.09),
+                      Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            model.authFormType == AuthFormType.login
+                                ? 'Or Login with'
+                                : 'Or Register with',
+                            style: Theme.of(context).textTheme.subtitle1,
+                          )),
+                      const SizedBox(height: 16.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.0),
+                              color: Colors.white,
+                            ),
+                            child: const Icon(Icons.add),
+                          ),
+                          const SizedBox(width: 16.0),
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.0),
+                              color: Colors.white,
+                            ),
+                            child: const Icon(Icons.add),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
