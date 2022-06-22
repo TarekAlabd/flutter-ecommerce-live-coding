cac168d21d9215d23676070e8d02f8fc16453f88
Esraa Mostfa <35074180+esraamostfa@users.noreply.github.com>
2022-06-22 17:06:27 +0200

    Update profile_page.dart

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/controllers/auth_controller.dart';
-import 'package:flutter_ecommerce/views/pages/auth_page.dart';
 import 'package:flutter_ecommerce/views/widgets/main_button.dart';
 import 'package:provider/provider.dart';
-import '../../services/auth.dart';
-import '../../utilities/enums.dart';
-import '../../utilities/routes.dart';
-import '../widgets/default_toast.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
 
-  Future<void> _logout(AuthController model, context) async {
+    Future<void> _logout(AuthController model, context) async {
     try {
       await model.logout();
-      const DefaultToast(message: 'successfully logged out', state: ToastStates.success)
-          .showToast();
-      Navigator.pushAndRemoveUntil(context,  MaterialPageRoute(
-          builder: (context) =>
-           const AuthPage()), (route) => false);
+      Navigator.pop(context);
     } catch (e) {
       debugPrint('logout error: $e');
-      const DefaultToast(message: 'something went wrong!', state: ToastStates.error)
-          .showToast();
     }
   }
 
