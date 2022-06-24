4bc4128f842f3785058cef99ef0a0d790684fc1f
esraamostfa <esraamostfa13@gmail.com>
2022-06-24 23:00:46 +0200

    [ENHANCEMENT] Make the show dialog as a separate component

--- lib/views/pages/auth_page.dart
+++ lib/views/pages/auth_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/controllers/auth_controller.dart';
 import 'package:flutter_ecommerce/utilities/enums.dart';
 import 'package:flutter_ecommerce/views/widgets/main_button.dart';
+import 'package:flutter_ecommerce/views/widgets/main_dialog.dart';
 import 'package:provider/provider.dart';
 
 class AuthPage extends StatefulWidget {
@@ -30,26 +31,7 @@ class _AuthPageState extends State<AuthPage> {
       await model.submit();
       if (!mounted) return;
     } catch (e) {
-      // TODO: We will refactor this code into another widget in the next session
-      showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-          title: Text(
-            'Error!',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          content: Text(
-            e.toString(),
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
+      MainDialog(context: context, title: 'Error', content: e.toString()).showAlertDialog();
     }
   }
 
