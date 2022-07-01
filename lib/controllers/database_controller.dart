3a964a2462e7b0e85fa54a3137412163c63543db
Tarek Alabd <tarekalabd1@gmail.com>
2022-07-01 16:57:11 +0200

    [FEATURE] add the business logic and api integration with firebase in the home page

--- lib/controllers/database_controller.dart
+++ lib/controllers/database_controller.dart
@@ -1,16 +1,28 @@
 import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/services/firestore_services.dart';
+import 'package:flutter_ecommerce/utilities/api_path.dart';
 
 abstract class Database {
-  Stream<List<Product>> productsStream();
+  Stream<List<Product>> salesProductsStream();
+  Stream<List<Product>> newProductsStream();
 }
 
 class FirestoreDatabase implements Database {
+  final String uid;
   final _service = FirestoreServices.instance;
 
+  FirestoreDatabase(this.uid);
+
   @override
-  Stream<List<Product>> productsStream() => _service.collectionsStream(
-        path: 'products/',
+  Stream<List<Product>> salesProductsStream() => _service.collectionsStream(
+        path: ApiPath.products(),
+        builder: (data, documentId) => Product.fromMap(data!, documentId),
+        queryBuilder: (query) => query.where('discountValue', isNotEqualTo: 0),
+      );
+
+  @override
+  Stream<List<Product>> newProductsStream() => _service.collectionsStream(
+        path: ApiPath.products(),
         builder: (data, documentId) => Product.fromMap(data!, documentId),
       );
 }
--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/controllers/database_controller.dart';
 import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/utilities/assets.dart';
 import 'package:flutter_ecommerce/views/widgets/list_item_home.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -48,6 +50,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final database = Provider.of<Database>(context);
+
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -95,17 +99,30 @@ class HomePage extends StatelessWidget {
                 const SizedBox(height: 8.0),
                 SizedBox(
                   height: 300,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: dummyProducts
-                        .map(
-                          (e) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListItemHome(product: e),
-                          ),
-                        )
-                        .toList(),
-                  ),
+                  child: StreamBuilder<List<Product>>(
+                      stream: database.salesProductsStream(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.active) {
+                          final products = snapshot.data;
+                          if (products == null || products.isEmpty) {
+                            return const Center(
+                              child: Text('No Data Available!'),
+                            );
+                          }
+                          return ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: products.length,
+                            itemBuilder: (_, int index) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ListItemHome(product: products[index]),
+                                  ),
+                          );
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }),
                 ),
                 _buildHeaderOfList(
                   context,
@@ -115,17 +132,30 @@ class HomePage extends StatelessWidget {
                 const SizedBox(height: 8.0),
                 SizedBox(
                   height: 300,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: dummyProducts
-                        .map(
-                          (e) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListItemHome(product: e),
-                          ),
-                        )
-                        .toList(),
-                  ),
+                  child: StreamBuilder<List<Product>>(
+                      stream: database.newProductsStream(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.active) {
+                          final products = snapshot.data;
+                          if (products == null || products.isEmpty) {
+                            return const Center(
+                              child: Text('No Data Available!'),
+                            );
+                          }
+                          return ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: products.length,
+                            itemBuilder: (_, int index) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ListItemHome(product: products[index]),
+                                  ),
+                          );
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }),
                 ),
               ],
             ),
