4cd62d59d46751c94feb430b6bd5e2820ce1964b
akhaled <akhaled@cairo.aas.com.sa>
2022-07-02 15:20:35 +0200

    Add rating bar, favorite button, prices and social media buttons on auth page

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,49 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/utilities/assets.dart';
+import 'package:flutter_ecommerce/views/widgets/header_of_list.dart';
 import 'package:flutter_ecommerce/views/widgets/list_item_home.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  /// TODO: Temp code till refactor to a separate class with a separate context
-  Widget _buildHeaderOfList(
-    BuildContext context, {
-    required String title,
-    VoidCallback? onTap,
-    required String description,
-  }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              title,
-              style: Theme.of(context).textTheme.headline4!.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-            ),
-            InkWell(
-              onTap: onTap,
-              child: Text(
-                'View All',
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-            ),
-          ],
-        ),
-        Text(
-          description,
-          style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                color: Colors.grey,
-              ),
-        ),
-      ],
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +33,9 @@ class HomePage extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0, vertical: 16.0),
+                  horizontal: 24.0,
+                  vertical: 16.0,
+                ),
                 child: Text(
                   'Street Clothes',
                   style: Theme.of(context).textTheme.headline4!.copyWith(
@@ -87,41 +51,48 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Column(
               children: [
-                _buildHeaderOfList(
-                  context,
+                HeaderOfList(
+                  onTap: () {},
                   title: 'Sale',
                   description: 'Super Summer Sale!!',
                 ),
                 const SizedBox(height: 8.0),
                 SizedBox(
-                  height: 300,
+                  height: size.height * 0.4,
                   child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: dummyProducts
                         .map(
                           (e) => Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: ListItemHome(product: e),
+                            child: ListItemHome(
+                              product: e,
+                              isNew: false,
+                            ),
                           ),
                         )
                         .toList(),
                   ),
                 ),
-                _buildHeaderOfList(
-                  context,
+                HeaderOfList(
+                  onTap: () {},
                   title: 'New',
                   description: 'Super New Products!!',
                 ),
                 const SizedBox(height: 8.0),
                 SizedBox(
-                  height: 300,
+                  height: size.height * 0.4,
                   child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: dummyProducts
                         .map(
                           (e) => Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: ListItemHome(product: e),
+                            child: ListItemHome(
+                              product: e,
+                              isNew: true,
+                              isFavorite: true,
+                            ),
                           ),
                         )
                         .toList(),
--- lib/views/widgets/list_item_home.dart
+++ lib/views/widgets/list_item_home.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/models/product.dart';
-import 'package:flutter_ecommerce/utilities/assets.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class ListItemHome extends StatelessWidget {
   final Product product;
-
-  const ListItemHome({
+  final bool isNew;
+  final VoidCallback? addToFavorites;
+  bool isFavorite;
+  ListItemHome({
     Key? key,
     required this.product,
+    required this.isNew,
+    this.addToFavorites,
+    this.isFavorite = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return DecoratedBox(
       decoration: BoxDecoration(),
       child: Column(
@@ -36,13 +42,13 @@ class ListItemHome extends StatelessWidget {
                   child: DecoratedBox(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(16.0),
-                      color: Colors.red,
+                      color: isNew ? Colors.black : Colors.red,
                     ),
                     child: Padding(
                       padding: const EdgeInsets.all(4.0),
                       child: Center(
                         child: Text(
-                          '${product.discountValue}%',
+                          isNew ? 'NEW' : '${product.discountValue}%',
                           style: Theme.of(context).textTheme.caption!.copyWith(
                                 color: Colors.white,
                               ),
@@ -52,7 +58,52 @@ class ListItemHome extends StatelessWidget {
                   ),
                 ),
               ),
+              Positioned(
+                left: size.width * 0.35,
+                bottom: 5,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        color: Colors.grey,
+                        spreadRadius: 2,
+                      )
+                    ],
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 25.0,
+                    child: InkWell(
+                      onTap: addToFavorites,
+                      child: Icon(
+                        Icons.favorite,
+                        size: 25.0,
+                        color: isFavorite ? Colors.red : Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
+          ),
+          const SizedBox(height: 8.0),
+          RatingBar.builder(
+            itemSize: 25.0,
+            initialRating: product.rate ?? 3,
+            minRating: 0,
+            direction: Axis.horizontal,
+            allowHalfRating: true,
+            itemCount: 5,
+            itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+            itemBuilder: (context, _) => const Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+            onRatingUpdate: (rating) {
+              debugPrint(rating.toString());
+            },
           ),
           const SizedBox(height: 8.0),
           Text(
@@ -69,12 +120,33 @@ class ListItemHome extends StatelessWidget {
                 ),
           ),
           const SizedBox(height: 6.0),
-          Text(
-            '${product.price}\$',
-            style: Theme.of(context).textTheme.subtitle2!.copyWith(
-              color: Colors.grey,
-            ),
-          ),
+          isNew
+              ? Text(
+                  '${product.price}\$',
+                  style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                        color: Colors.grey,
+                      ),
+                )
+              : Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: '${product.price}\$  ',
+                        style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough,
+                            ),
+                      ),
+                      TextSpan(
+                        text:
+                            '  ${product.price * (product.discountValue!) / 100}\$',
+                        style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                              color: Colors.red,
+                            ),
+                      ),
+                    ],
+                  ),
+                )
         ],
       ),
     );
