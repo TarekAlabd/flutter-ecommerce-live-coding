e2d0f64a30c83f4b184157421c6e800f1d52e080
Tarek Alabd <tarekalabd1@gmail.com>
2022-07-22 15:43:54 +0200

    Day5 1/7 home business logic (#21)
    * [FEATURE] add the business logic and api integration with firebase in the home page
    
    * [ENHANCEMENT] refactor the list header to a separate component (#17)
    
    Co-authored-by: Esraa Mostfa <35074180+esraamostfa@users.noreply.github.com>

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,53 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/controllers/database_controller.dart';
 import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/utilities/assets.dart';
+import 'package:flutter_ecommerce/views/widgets/list_header.dart';
 import 'package:flutter_ecommerce/views/widgets/list_item_home.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  /// TODO: Temp code till refactor to a separate class with a separate context
-  Widget _buildHeaderOfList(
-    BuildContext context, {
-    required String title,
-    VoidCallback? onTap,
-    required String description,
-  }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              title,
-              style: Theme.of(context).textTheme.headline4!.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-            ),
-            InkWell(
-              onTap: onTap,
-              child: Text(
-                'View All',
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-            ),
-          ],
-        ),
-        Text(
-          description,
-          style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                color: Colors.grey,
-              ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final database = Provider.of<Database>(context);
+
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -87,45 +53,69 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 24.0),
             child: Column(
               children: [
-                _buildHeaderOfList(
-                  context,
+                const ListHeader(
                   title: 'Sale',
                   description: 'Super Summer Sale!!',
                 ),
                 const SizedBox(height: 8.0),
                 SizedBox(
                   height: 300,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: dummyProducts
-                        .map(
-                          (e) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListItemHome(product: e),
-                          ),
-                        )
-                        .toList(),
-                  ),
+                  child: StreamBuilder<List<Product>>(
+                      stream: database.salesProductsStream(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.active) {
+                          final products = snapshot.data;
+                          if (products == null || products.isEmpty) {
+                            return const Center(
+                              child: Text('No Data Available!'),
+                            );
+                          }
+                          return ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: products.length,
+                            itemBuilder: (_, int index) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ListItemHome(product: products[index]),
+                                  ),
+                          );
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }),
                 ),
-                _buildHeaderOfList(
-                  context,
+                const ListHeader(
                   title: 'New',
                   description: 'Super New Products!!',
                 ),
                 const SizedBox(height: 8.0),
                 SizedBox(
                   height: 300,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: dummyProducts
-                        .map(
-                          (e) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListItemHome(product: e),
-                          ),
-                        )
-                        .toList(),
-                  ),
+                  child: StreamBuilder<List<Product>>(
+                      stream: database.newProductsStream(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.active) {
+                          final products = snapshot.data;
+                          if (products == null || products.isEmpty) {
+                            return const Center(
+                              child: Text('No Data Available!'),
+                            );
+                          }
+                          return ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: products.length,
+                            itemBuilder: (_, int index) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ListItemHome(product: products[index]),
+                                  ),
+                          );
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }),
                 ),
               ],
             ),
